f79d445b9429c7113bf154ae684d395392d45767
Alexander Torres <alexanderdtorres84@gmail.com>
2026-01-06 09:45:30 -0800

    brugster.com

---
+++ app/src/main/java/com/example/heroes/Hero.kt
@@ -0,0 +1,3 @@
+package com.example.heroes
+
+data class Hero(val name: String, val desc: String, val superpower : String, val ranking : String, val image: String )
---
+++ app/src/main/java/com/example/heroes/HeroesDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.example.heroes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.heroes.databinding.ActivityHeroesDetailBinding
+
+class HeroesDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHeroesDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityHeroesDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
